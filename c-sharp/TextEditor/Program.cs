f1887315537ce902816d9da1845af0f47e7b4aa1
JPMSilvaFL <joao.pedro.69461@gmail.com>
2025-04-09 19:50:15 -0300

    textEditor -- v01

--- c-sharp/TextEditor/Program.cs
+++ c-sharp/TextEditor/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class TextEditor{
+    static void Main(string[] main) {
+        int x = 0;
+        do {
+            Console.Clear();
+            Console.WriteLine("Escolha uma das opções:");
+            Console.WriteLine("1- Editar");
+            Console.WriteLine("2- Criar");
+            Console.WriteLine("0- Sair");
+            Console.WriteLine("----------------------------");
+            x = int.Parse(Console.ReadLine());
+
+            switch (x) {
+                case 1: Console.WriteLine("Editado!!"); break;
+                case 2: Console.WriteLine("Criado!!"); break;
+            }
+            Thread.Sleep(1000);
+        }
+        while (x != 0);
+    }
+}
