42be85be45e336ae2b5fcde4df026d161d7dc101
JPMSilvaFL <joao.pedro.69461@gmail.com>
2025-04-08 23:29:49 -0300

    Stopwatch

---
+++ c-sharp/Stopwatch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+namespace Stopwatch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Quantos segundos o cronometro continuara?");
+        int t = int.Parse(Console.ReadLine());
+
+
+        int i = 0;
+        int tempoG = 0;
+        while (tempoG <= t)
+        {
+            for (int j = 0; j <= 59; j++)
+            {
+                for (int k = 0; k <= 59; k++)
+                {
+                    tempoG++;
+                    Console.Clear();
+                    if (tempoG == t)
+                    {
+                        Console.WriteLine(i + ":" + j + ":" + k);
+                        return;
+                    }
+                    Console.WriteLine(i + ":" + j + ":" + k);
+                    Thread.Sleep(1);
+                }
+            }
+        i++;
+        }
+
+    }
+}
+
